e144cc041ae69017aaccf9cf61acb78e7117e583
Io Astorga <patitoalreves@gmail.com>
2015-05-20 15:32:04 -0700

    Part 1 v1

--- k_means.py
+++ k_means.py
@@ -1,4 +1,5 @@
-from utils import * 
+from utils import *
+import numpy as np
 
 
 def computeSSE(data, centers, clusterID):
@@ -12,12 +13,15 @@ def computeSSE(data, centers, clusterID):
 
 def updateClusterID(data, centers):
     nData = len(data) 
-    
-    clusterID = [0] * nData
-    
-    # TODO 
-    # assign the closet center to each data point
+    nCenters = len(centers)
 
+    clusterID = [0] * nData
+
+    # assign the closet center to each data point
+    for i in range(nData):
+        for j in range(nCenters):
+            DisToCenter[j] = squaredDistance(data[i], centers[j])
+        clusterID[i] = DisToCenter.index(min(DistToCenter))
     
     return clusterID
 
@@ -25,9 +29,14 @@ def updateClusterID(data, centers):
 def updateCenters(data, clusterID, K):
     nDim = len(data[0])
     centers = [[0] * nDim for i in range(K)]
-
-    # TODO recompute the centers based on current clustering assignment
+    
+    # Recompute the centers based on current clustering assignment
     # If a cluster doesn't have any data points, in this homework, leave it to ALL 0s
+
+    for j in range(K):
+        searchval = K[j] 
+        k_indices = np.where(clusterID == searchval)[0]
+        centers[j] = np.sum(data(k_indices), axis=0)/len(k_indices)
 
     return centers 
 
